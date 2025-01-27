12f3ecbdfaba0ade498c678d9889dcaafce3304a
KRISHNARAJ D <119559695+KRISHNARAJ-D@users.noreply.github.com>
2025-01-27 21:30:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,5 +144,8 @@ def img_to_pdf():
     # Automatically trigger the download of the PDF
     return send_file(pdf_output, as_attachment=True)
 
+# Run the Flask app with Vercel compatibility
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
